bafa035a16d0e33da28c2c49ee590bae7d84f9ea
885685845 <96099327+RoshanIndane@users.noreply.github.com>
2023-02-23 00:45:57 +0530

    mapchar  problem

---
+++ mapCharacters.js
@@ -0,0 +1,21 @@
+
+function mapChar(N){
+    let string="abcdefghijklmnopqrstuvwxyz";
+    let value=0;
+    let output=0;
+    
+    for(var r=0; r<N; r++){
+        value=r+1;
+        output=string[r]+ "-"+value;
+        console.log(output);
+    }
+}
+    
+mapChar(5)
+// output=>a-1
+//         b-2
+//         c-3
+//         d-4
+//         e-5
+
+mapChar(25)
