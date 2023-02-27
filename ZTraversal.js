02e771236108ce66dabbf185b74f76a0f9cb2a25
885685845 <96099327+RoshanIndane@users.noreply.github.com>
2023-02-28 01:28:08 +0530

    dsa

---
+++ ZTraversal.js
@@ -0,0 +1,22 @@
+
+function zTraversal(N, arr) {
+    var bag = '';
+    for (var c = 0; c < N - 1; c++) {
+        bag = bag + arr[0][c] + ' ';
+    }
+    for (var c = N - 1; c >= 0; c--) {
+        for (var r = 0; r < N; r++) {
+            if (r + c == N - 1) {
+                bag = bag + arr[r][c] + ' ';
+            }
+        }
+    }
+    for (var c = 1; c < N; c++) {
+        bag = bag + arr[N - 1][c] + ' ';
+    }
+    console.log(bag);
+}
+
+zTraversal(3, [[1, 2, 3],
+[4, 5, 6],
+[7, 8, 9]])  // 1 2 3 5 7 8 9
