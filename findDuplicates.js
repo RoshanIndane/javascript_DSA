8f5da5832c76c938a64cd68df8a7daaef426cab4
885685845 <96099327+RoshanIndane@users.noreply.github.com>
2023-03-10 00:45:55 +0530

    addd

---
+++ findDuplicates.js
@@ -0,0 +1,18 @@
+function findDuplicates(arr){
+
+    var newArr=[];
+
+    for(var r=0; r<arr.length; r++){
+        for(var s=r+1; s<arr.length; s++){
+            if(r!=s){
+                if(arr[r]===arr[s]){
+                    newArr.push(arr[r])
+                }
+            }
+        }
+    }
+    console.log(newArr)
+
+}
+
+findDuplicates([1, 2, 3, 6, 3, 6, 1])
